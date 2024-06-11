8c1e9a82b0b10066551633c99c4788783680306f
EmmaSinibaldi <emma.sinibaldi@etu.umontpellier.fr>
2024-06-11 10:58:28 +0200

    modif path

--- scriptR/donnees.R
+++ scriptR/donnees.R
@@ -6,12 +6,12 @@ library(dplyr)
 library(readxl)
 
 
-data_Sene <- read_excel("C:/Users/sinibaldi/Desktop/stage/dataframes/data_Sene.xlsx")
-data_Burk <- read_excel("C:/Users/sinibaldi/Desktop/stage/dataframes/data_Burk.xlsx")
-data_Mali <- read_excel("C:/Users/sinibaldi/Desktop/stage/dataframes/data_Mali.xlsx")
+data_Sene <- read_excel("data/data_Sene.xlsx")
+data_Burk <- read_excel("data/data_Burk.xlsx")
+data_Mali <- read_excel("data/data_Mali.xlsx")
 
 donnees <- rbind(data_Sene, data_Burk, data_Mali)
-
+donnees[, 6:16] <- lapply(donnees[, 6:16], as.numeric)
 
 
 
